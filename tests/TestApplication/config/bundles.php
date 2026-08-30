de714ad68e8bd5c07f5cc9c71cc19026623029b2
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2026-08-30 09:19:28 +0200

    Add Wishlist plugin dependency

--- tests/TestApplication/config/bundles.php
+++ tests/TestApplication/config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    Sylius\WishlistPlugin\SyliusWishlistPlugin::class => ['all' => true],
     BitExpert\SyliusWishlistConciergePlugin\BitExpertSyliusWishlistConciergePlugin::class => ['all' => true],
 ];
